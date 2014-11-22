7a3f945814faa482360e812e3f2038cf30420f29
Alexandre Bayle <alex.bayle.6691@gmail.com>
2014-11-22 17:55:02 +0100

    boolean

---
+++ src/BooleanValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Alexandre
+ * Date: 22/11/14
+ * Time: 17:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace AB\Validator;
+
+
+class BooleanValidator {
+
+}
